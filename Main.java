3a2d78cfe736ad4a716ac2467848c07c669f9bb4
Vampsas <43552520+Vampsas@users.noreply.github.com>
2019-02-25 18:36:40 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,50 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter symbol (+, -, %, * or /) and two numbers separeted by spaces :");
+        System.out.println("Enter  numbers separeted by spaces :");
+        String s = sc.nextLine();
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(s.split(" ")));
+        Collections.reverse(list);
 
-        String text;
 
-        while((text = sc.readLine()) != null && text.length() > 0) {
+        for(String k : list) {
+            System.out.print(k);
+            System.out.print(" ");
 
-            ArrayList<String> list = new ArrayList<String>(Arrays.asList(text.split(" ")));
-
-            char symbol = list.get(0).charAt(0);
-
-            int num1 = Integer.parseInt(list.get(1));
-            int num2 = Integer.parseInt(list.get(2));
-            int sum, substraction, multi, division, rest;
-
-           switch (sumbol){
-
-               case '+' : sum = num1 + num2;
-                   System.out.println(sum);
-                   break;
-               case '-': substraction = num1 - num2;
-                   System.out.println(substraction);
-                   break;
-               case '*': multi = num1 * num2;
-                   System.out.println(multi);
-                   break;
-               case '/': division = num1 / num2;
-                   System.out.println(division);
-                   break;
-               case '%': rest = num1 % num2;
-                   System.out.println(rest);
-                   break;
-
-                   default: break;
-                 }
-                list.clear();
-            }
         }
     }
+}
